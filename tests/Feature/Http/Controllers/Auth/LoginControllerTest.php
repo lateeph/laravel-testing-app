b865aab4b40e23ec7a9a637a9af66583bc2412b8
Abdul-Latif Sumani <latifsumani1@gmail.com>
2020-01-09 12:12:36 +0000

    first commit

---
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    /** @test */
+    //public function testExample()
+    public function login_displays_the_login_form()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+
+    /** @test */
+    public function login_displays_validation_errors()
+    {
+        $response = $this->post('/login', []);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    public function login_authenticates_and_redirects_user()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->post(route('login'), [
+            'email'  => $user->email,
+            'password'  =>  'password'
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $this->assertAuthenticationAs($user);
+    }
+
+    /** @test */
+    public function register_creates_and_authenticates_a_user()
+    {
+        $name = $this->faker->name;
+        $email = $this->faker->safeEmail;
+        $password = $this->faker->password(8);
+
+        $response = $this->post('register', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password,
+        ]);
+
+        $response->assertRedirect(route('home'));
+
+        $user = User::where('email', $email)->where('name', $name)->first();
+        $this->assertNotNull($user);
+
+        $this->assertAuthenticatedAs($user);
+    }
+    /*{
+        # code...
+    }
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }*/
+}
